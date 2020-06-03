61a9908de26fc5637ceaf431660c79192975aac2
Takuto Nanjo <5742327-takun@users.noreply.gitlab.com>
2020-06-03 19:02:18 +0900

    add a comment

--- on-zfs.sh
+++ on-zfs.sh
@@ -759,7 +759,10 @@ function create_zfs_partitions {
 	zfs create -o mountpoint=/snap           ${v_rpool_name}/snap
 	zfs create -o mountpoint=/srv            ${v_rpool_name}/srv
 	zfs create -o mountpoint=/tmp            ${v_rpool_name}/tmp
+
+	# If you make /usr as a ZFS partition, you cant boot your system.
 	zfs create -o canmount=off               ${v_rpool_name}/usr      # Just to make a parent.
+
 	zfs create -o mountpoint=/usr/local      ${v_rpool_name}/usr/local
 	zfs create -o mountpoint=/var            ${v_rpool_name}/var
 	zfs create -o canmount=off               ${v_rpool_name}/var/lib  # Just to make a parent.
