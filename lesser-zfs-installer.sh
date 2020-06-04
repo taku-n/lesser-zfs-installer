dec27cf85298962c824b0b08fdf9b75e06e9ee2d
Takuto Nanjo <5742327-takun@users.noreply.gitlab.com>
2020-06-04 17:35:09 +0900

    add a message

--- lesser-zfs-installer.sh
+++ lesser-zfs-installer.sh
@@ -543,6 +543,8 @@ function setup_partitions {
 		sgdisk -n 5::                            -t 5:8300 "$selected_disk" # Linux File Sys
 	fi
 
+	echo "If blockdev fails, rerun this script."
+
 	blockdev --rereadpt $selected_disk
 
   # The partition symlinks are not immediately created, so we wait.
@@ -586,7 +588,7 @@ function setup_partitions {
 		temp_partition=${selected_disk}-part5
 	fi
 
-	echo "If mkfs.fat and mkswap fail, rerun this script."
+	echo "If mkfs.fat or mkswap fail, rerun this script."
 
 	if [ $v_swap_size -eq 0 ]; then
 		mkfs.fat -F 32 -n ESP $efi_partition
